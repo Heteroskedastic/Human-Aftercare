693219308f12d3af40fdd2b208525c4181b59d9f
pprolancer <pprolancer@gmail.com>
2021-06-06 07:06:07 -0700

    -

--- human_aftercare/apps/facilities/admin.py
+++ human_aftercare/apps/facilities/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django_tenants.utils import get_public_schema_name
 
 from .models import Facility, FacilityDomain
 
@@ -11,6 +12,10 @@ class FacilityAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'schema_name', 'description', 'email', 'phone_number', 'is_active')
     search_fields = ('name', 'schema_name', 'description', 'email', 'phone_number')
     inlines = [DomainInline]
+
+    def get_queryset(self, request):
+        public_schema = get_public_schema_name()
+        return super().get_queryset(request).exclude(schema_name=public_schema)
 
 
 admin.site.register(Facility, FacilityAdmin)
