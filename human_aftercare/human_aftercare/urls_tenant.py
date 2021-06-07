c50477f1088cbf50f3b6c5237f976c2d5e1bf0f2
pprolancer <pprolancer@gmail.com>
2021-06-07 06:12:28 -0700

    -

--- human_aftercare/human_aftercare/urls_tenant.py
+++ human_aftercare/human_aftercare/urls_tenant.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls import handler404
 from django.views.static import serve
 from django.urls import path, re_path, include
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
